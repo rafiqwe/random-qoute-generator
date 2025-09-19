b5886ffcefbfed9852489af3122095c440f6d608
rafiqwe <rmlmanik502@gmail.com>
2025-09-19 20:30:38 +0600

    Completed creating random qoute generator project

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,18 @@
 console.log('hellow');
 const generateBtn = document.querySelector('#generateBtn');
 const qouteElm = document.querySelector('#qoute');
+const author = document.querySelector('#author');
 generateBtn?.addEventListener('click', async () => {
     try {
-        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=success', {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes', {
             headers: {
-                "X-Api-Key": 'wKSTCHnr40KxODQzgcmjoA==8BoHnZu1wlfeFQmR'
+                'X-Api-Key': 'wKSTCHnr40KxODQzgcmjoA==8BoHnZu1wlfeFQmR'
             }
         });
-        const data = res.json();
+        const data = await res.json();
         console.log(data);
+        qouteElm.textContent = data[0]?.quote,
+            author.innerText = 'Author:' + " " + data[0]?.author;
     }
     catch (error) {
         console.error("fatch api error:", error);
--- src/script.ts
+++ src/script.ts
@@ -2,18 +2,21 @@ console.log('hellow');
 
 const generateBtn = document.querySelector<HTMLButtonElement>('#generateBtn');
 const qouteElm = document.querySelector<HTMLHeadingElement>('#qoute');
+const author = document.querySelector<HTMLHeadingElement>('#author')
 
 
 generateBtn?.addEventListener('click',async () => {
     try {
-        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=success',{
-            headers:{
-                "X-Api-Key": 'wKSTCHnr40KxODQzgcmjoA==8BoHnZu1wlfeFQmR'
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes', {
+            headers: {
+                'X-Api-Key': 'wKSTCHnr40KxODQzgcmjoA==8BoHnZu1wlfeFQmR'
             }
-        })
-        const data = res.json()
+        });
+        const data = await res.json();
         console.log(data);
         
+        qouteElm.textContent = data[0]?.quote,
+        author.innerText = 'Author:' + " " + data[0]?.author              
     } catch (error) {
         console.error("fatch api error:", error);
     }
